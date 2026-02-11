6be38c776c36b9fff5d426a78b887d16a17a578b
Chinmay731-gi <chinmayihare@gmail.com>
2026-02-11 12:06:36 +0530

    Delete Url-Shortner directory

--- Url-Shortner/routes/staticRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Url = require('../models/url');
-const router = express.Router();
-router.get('/', async(req,res) => {
-    const allUrls = await Url.find({});
-    return res.render('home', {Urls: allUrls});
-})
-module.exports = router;    
--- Url-Shortner/routes/url.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {handleGenerateShortUrl , handleGetAnalytics} = require('../controllers/url');
-
-router.post('/', handleGenerateShortUrl);
-
-router.get('/analytics/:shortId',handleGetAnalytics);
-
-module.exports = router;    
